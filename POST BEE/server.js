8b4491dd7d7e919703d67e6ad9c90288f3ca1bdb
ronak1114 <ronak1114.be23@chitkara.edu.in>
2025-03-03 16:33:46 +0530

    POST REQUEST COMMIT MONDAY

--- POST BEE/server.js
+++ POST BEE/server.js
@@ -1,12 +1,31 @@
+//Post request: if you want to change the state of the server
+//data is not visible in the url
+
 const express = require("express");
 const app = express();
 
+//express.urlencoded : It is a Middleware that passes urlencoded data into object
 
-app.get("/adduser",(req,res)=>{
-    res.sendFile(__dirname+"/register.html");
+app.use(express.urlencoded({extended : true}))
+
+let userdata = [];
+
+app.get("/adduser", (req, res) => {
+    res.sendFile(__dirname + "/register.html");
 });
 
-
-app.listen(4020,()=>{
-    console.log("SERVER STARTED...");
+app.post("/adduser", (req, res) => {
+    let {name, email, password} = req.body;
+    let newUser = {
+        name : name,
+        email : email,
+        password : password
+    };
+    userdata.push(newUser);
+    //res.send(<center><h1>User Added</h1> <br/> ${userdata} </center>);
+    res.send(userdata);
+    console.log(name, email, password);
+});
+app.listen(4020, ()=>{
+    console.log("Server started on Port 4020...");
 });
